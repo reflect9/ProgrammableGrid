05a6934c901cd9f8cffc63e92448c470a41198fe
reflect9 <reflect9@gmail.com>
2013-11-25 20:20:41 -0500

    aa

--- js/problems.js
+++ js/problems.js
@@ -83,16 +83,63 @@ pg.problems = {
 				A:null,
 			}
 		];
-		var goal_nodes = [
+		var goal_node = 
 			{	V:value_pdf_modified,
+				P:null,
+				I:null,
+				A:null,
+			};
+		return [initial_nodes, goal_node];
+	},
+	'modify_element_attribute': function() {
+		// initialize page and initial node set
+		BASE_URL = 'http://scholar.google.com/scholar?q=ctarcade&btnG=&hl=en&as_sdt=0%2C21v';
+		if(window.location.href != BASE_URL) {
+			window.location.replace(BASE_URL);
+			console.log("try again in this page.");
+			return;
+		}
+		var value_body = $("body");
+		var value_articles = $(".gs_r"); 
+		var value_pdf = $(".gs_md_wp"); 
+		var value_pdf_modified = _.map(value_pdf, function(node, index) {
+			var article_el = $(node).parents(".gs_r");
+			var title = $(article_el).find("h3.gs_rt>a").text();
+			// title = title.replace(/\W/g,"-");
+			var author_name = $(article_el).find(".gs_a").text();
+			// first_author = author_name.replace(/[,-].*/g,"").replace(/ /g,"");
+			// first_author = author_name;
+			// var year = author_name.match(/\d{4}/);
+			// var year = author_name;
+			var file_name = title+"^"+author_name
+			$(node).attr("download",file_name);
+			return $(node).get(0);
+		}); 
+		var value_download_text = _.map(value_pdf_modified, function(node){
+			return $(node).attr("download");
+		});
+		var initial_nodes = [
+			{	V:value_body,
 				P:null,
 				I:null,
 				A:null,
 			}
 		];
-		return [initial_nodes, goal_nodes];
+		var goal_node = 
+			{	V:value_pdf_modified,
+				P:null,
+				I:null,
+				A:null,
+			};
+		// run planner
+		return [initial_nodes, goal_node];
+
+
 
 	},
+
+
+
 	'scholar_extract_title': function() {
 		BASE_URL = 'http://scholar.google.com/scholar?q=ctarcade&btnG=&hl=en&as_sdt=0%2C21v';
 		if(window.location.href != BASE_URL) {
